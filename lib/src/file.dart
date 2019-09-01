8053fca794bce04a4e0bdd4350abfe6f75928299
zhoubokun <zhoubokun@bytedance.com>
2019-09-01 16:38:41 +0800

    fit jianguoyun webdav
    Change-Id: I4c83cc5280445585b304f15e2145f7b7805f1d3b

--- lib/src/file.dart
+++ lib/src/file.dart
@@ -4,38 +4,30 @@ class FileInfo {
   String path;
   String size;
   String modificationTime;
-  DateTime creationTime;
   String contentType;
 
-  FileInfo(this.path, this.size, this.modificationTime, this.creationTime,
-      this.contentType);
+  FileInfo(this.path, this.size, this.modificationTime, this.contentType);
 
   // Returns the decoded name of the file / folder without the whole path
   String get name {
-    if (this.isDirectory) {
-      return Uri.decodeFull(this.path
-          .substring(0, this.path.lastIndexOf("/"))
-          .split("/")
-          .last);
-    }
-
-    return Uri.decodeFull(this.path
+    var path = !this.path.endsWith("/") ? this.path : this.path.substring(0, this.path.length - 1);
+    return Uri.decodeFull(path
         .split("/")
         .last);
   }
 
-  bool get isDirectory => this.path.endsWith("/");
+  bool get isDirectory => this.contentType == "httpd/unix-directory";
 
   @override
   String toString() {
-    return 'FileInfo{name: $name, isDirectory: $isDirectory ,path: $path, size: $size, modificationTime: $modificationTime, creationTime: $creationTime, contentType: $contentType}';
+    return 'FileInfo{name: $name, isDirectory: $isDirectory ,path: $path, size: $size, modificationTime: $modificationTime, contentType: $contentType}';
   }
 }
 
 /// get filed [name] from the property node
 String prop(dynamic prop, String name, [String defaultVal]) {
   if (prop is Map) {
-    final val = prop['D:' + name];
+    final val = prop['d:' + name];
     if (val == null) {
       return defaultVal;
     }
@@ -52,32 +44,31 @@ List<FileInfo> treeFromWevDavXml(String xmlStr) {
   var xmlDocument = xml.parse(xmlStr);
 
   // Iterate over the response to find all folders / files and parse the information
-  xmlDocument.findAllElements("D:response").forEach((response) {
-    var davItemName = response.findElements("D:href").single.text;
+  xmlDocument.findAllElements("d:response").forEach((response) {
+    var davItemName = response.findElements("d:href").single.text;
     response
-        .findElements("D:propstat")
+        .findElements("d:propstat")
         .single
-        .findElements("D:prop")
+        .findElements("d:prop")
         .forEach((element) {
       var contentLength =
           element
-              .findElements("D:getcontentlength")
+              .findElements("d:getcontentlength")
               .single
               .text;
 
       var lastModified = element
-          .findElements("D:getlastmodified")
+          .findElements("d:getlastmodified")
           .single
           .text;
-
-      var creationTime = element
-          .findElements("D:creationdate")
+      
+      var contentType = element
+          .findElements("d:getcontenttype")
           .single
           .text;
 
       // Add the just found file to the tree
-      tree.add(new FileInfo(davItemName, contentLength, lastModified,
-          DateTime.parse(creationTime), ""));
+      tree.add(new FileInfo(davItemName, contentLength, lastModified, contentType));
     });
   });
 
